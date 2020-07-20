f6c46550ef1206969f046bf12dd8b38f1ee93deb
RebeccaMillwood <bec-millwood@live.com.au>
2020-07-20 22:59:38 +1000

    programmed question 1 and 2 of exercise 1

---
+++ variables-and-user-input-exercises.py
@@ -0,0 +1,37 @@
+# Question 1:
+
+# first_int = input("enter a number")
+# second_int = input("enter another number")
+
+# print(type(first_int))
+
+# result = int(first_int) + int(second_int)
+
+# print(result)
+
+
+# first_float = input("enter a number")
+# second_int = input("enter another number")
+
+
+# result = float(first_float) + int(second_int) 
+
+# print(result)
+
+# # Question 2:
+
+# first_int = input("enter a number")
+# second_int = input("enter another number")
+
+# result = int(first_int) * int(second_int)
+
+# print(result)
+
+
+# first_float = input("enter a number")
+# second_int = input("enter another number")
+
+
+# result = float(first_float) * int(second_int) 
+
+# print(result)
