18516ed666244cd04b7fb703fa195f5539474f8c
RebeccaMillwood <bec-millwood@live.com.au>
2020-07-20 23:42:23 +1000

    added space at end of input question, for questions 1 & 2, programmed question 4

--- variables-and-user-input-exercises.py
+++ variables-and-user-input-exercises.py
@@ -1,7 +1,7 @@
 # Question 1:
 
-# first_int = input("enter a number")
-# second_int = input("enter another number")
+# first_int = input("enter a number ")
+# second_int = input("enter another number ")
 
 # print(type(first_int))
 
@@ -10,8 +10,8 @@
 # print(result)
 
 
-# first_float = input("enter a number")
-# second_int = input("enter another number")
+# first_float = input("enter a number ")
+# second_int = input("enter another number ")
 
 
 # result = float(first_float) + int(second_int) 
@@ -20,16 +20,16 @@
 
 # # Question 2:
 
-# first_int = input("enter a number")
-# second_int = input("enter another number")
+# first_int = input("enter a number ")
+# second_int = input("enter another number ")
 
 # result = int(first_int) * int(second_int)
 
 # print(result)
 
 
-# first_float = input("enter a number")
-# second_int = input("enter another number")
+# first_float = input("enter a number ")
+# second_int = input("enter another number ")
 
 
 # result = float(first_float) * int(second_int) 
@@ -38,18 +38,25 @@
 
 # Question 3: 
 
-kilometers = input("enter number of kilometers, e.g 10 ")
+# kilometers = input("enter number of kilometers, e.g 10 ")
 
-result_m = int(kilometers) * 1000
-result_cm = int(kilometers) * 100000
+# result_m = int(kilometers) * 1000
+# result_cm = int(kilometers) * 100000
 
-print(f"{kilometers}km = {result_m}m")
-print(f"{kilometers}km = {result_cm}cm")
+# print(f"{kilometers}km = {result_m}m")
+# print(f"{kilometers}km = {result_cm}cm")
 
-kilometers = input("enter number of kilometers, e.g 10 ")
+# kilometers = input("enter number of kilometers, e.g 10 ")
 
-result_m = float(kilometers) * 1000
-result_cm = float(kilometers) * 100000
+# result_m = float(kilometers) * 1000
+# result_cm = float(kilometers) * 100000
 
-print(f"{kilometers}km = {result_m}m")
-print(f"{kilometers}km = {result_cm}cm")
+# print(f"{kilometers}km = {result_m}m")
+# print(f"{kilometers}km = {result_cm}cm")
+
+# Question 4:
+
+name = input("What is your name? ")
+height = input("What is your height? (numbers only) ")
+
+print(f"{name} is {height}cms tall.")
