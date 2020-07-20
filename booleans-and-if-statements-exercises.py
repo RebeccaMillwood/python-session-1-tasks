bae6225ae51822d4c12c1483c4273cb282e0ecf0
RebeccaMillwood <bec-millwood@live.com.au>
2020-07-21 00:20:43 +1000

    programmed question 2 for exercise 2

--- booleans-and-if-statements-exercises.py
+++ booleans-and-if-statements-exercises.py
@@ -1,13 +1,28 @@
 # Question 1:
 # input True or False
 
+# moths_in_house = False
+
+# if moths_in_house:
+#     (print("Get the moths!"))
+
+# else:
+#     (print("No threats detected"))    
+
+# Question 2:
+# change True or False combos for different outputs
+
 moths_in_house = False
+mitch_is_home = True
 
-if moths_in_house:
-    (print("Get the moths!"))
+if moths_in_house and mitch_is_home:
+    print("Hooman! Help me get the moths")
 
-else:
-    (print("No threats detected"))    
+elif not moths_in_house and not mitch_is_home:  
+    print("No threats detected") 
 
+elif moths_in_house and not mitch_is_home:
+    print("Meooooooooooow! Hisssss!")
 
-
+elif not moths_in_house and mitch_is_home:
+    print("Climb on Mitch")        
