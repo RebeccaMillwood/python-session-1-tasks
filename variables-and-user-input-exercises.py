e007cd6e9a50e2e6b77445a1750f249bc54c3455
RebeccaMillwood <bec-millwood@live.com.au>
2020-07-20 23:44:09 +1000

    commented out headings

--- variables-and-user-input-exercises.py
+++ variables-and-user-input-exercises.py
@@ -1,58 +1,58 @@
 # Question 1:
 
-# first_int = input("enter a number ")
-# second_int = input("enter another number ")
+first_int = input("enter a number ")
+second_int = input("enter another number ")
 
-# print(type(first_int))
+print(type(first_int))
 
-# result = int(first_int) + int(second_int)
+result = int(first_int) + int(second_int)
 
-# print(result)
-
-
-# first_float = input("enter a number ")
-# second_int = input("enter another number ")
+print(result)
 
 
-# result = float(first_float) + int(second_int) 
-
-# print(result)
-
-# # Question 2:
-
-# first_int = input("enter a number ")
-# second_int = input("enter another number ")
-
-# result = int(first_int) * int(second_int)
-
-# print(result)
+first_float = input("enter a number ")
+second_int = input("enter another number ")
 
 
-# first_float = input("enter a number ")
-# second_int = input("enter another number ")
+result = float(first_float) + int(second_int) 
+
+print(result)
+
+# Question 2:
+
+first_int = input("enter a number ")
+second_int = input("enter another number ")
+
+result = int(first_int) * int(second_int)
+
+print(result)
 
 
-# result = float(first_float) * int(second_int) 
+first_float = input("enter a number ")
+second_int = input("enter another number ")
 
-# print(result)
+
+result = float(first_float) * int(second_int) 
+
+print(result)
 
 # Question 3: 
 
-# kilometers = input("enter number of kilometers, e.g 10 ")
+kilometers = input("enter number of kilometers, e.g 10 ")
 
-# result_m = int(kilometers) * 1000
-# result_cm = int(kilometers) * 100000
+result_m = int(kilometers) * 1000
+result_cm = int(kilometers) * 100000
 
-# print(f"{kilometers}km = {result_m}m")
-# print(f"{kilometers}km = {result_cm}cm")
+print(f"{kilometers}km = {result_m}m")
+print(f"{kilometers}km = {result_cm}cm")
 
-# kilometers = input("enter number of kilometers, e.g 10 ")
+kilometers = input("enter number of kilometers, e.g 10 ")
 
-# result_m = float(kilometers) * 1000
-# result_cm = float(kilometers) * 100000
+result_m = float(kilometers) * 1000
+result_cm = float(kilometers) * 100000
 
-# print(f"{kilometers}km = {result_m}m")
-# print(f"{kilometers}km = {result_cm}cm")
+print(f"{kilometers}km = {result_m}m")
+print(f"{kilometers}km = {result_cm}cm")
 
 # Question 4:
 
